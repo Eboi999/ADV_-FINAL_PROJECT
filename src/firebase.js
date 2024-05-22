14914546d1a879b970d399b06f999e4708957d37
Eboi999 <clarencedave.banagaso@hcdc.edu.ph>
2024-05-23 01:22:31 +0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmVFcbmocBNS0_9olpfr_AXa3JrAv3qSA",
+  authDomain: "advproject-78fb7.firebaseapp.com",
+  projectId: "advproject-78fb7",
+  storageBucket: "advproject-78fb7.appspot.com",
+  messagingSenderId: "495193620792",
+  appId: "1:495193620792:web:128b5999476f8f26bc01cd"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
